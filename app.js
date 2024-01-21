76706f1e18bef0f915c7cd5271ecbdf62c4be35b
Mariel <99100374+mariel-rs@users.noreply.github.com>
2024-01-21 14:36:51 -0600

    Complete exercises

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+let title = document.querySelector("h1");
+title.innerHTML = "Hora del desafío";
 
+function clickedButton() {
+    console.log("El botón fue clicado");
+}
+
+function promptButton() {
+    let brazilCity = prompt("Escribe el nombre de una ciudad de Brasil");
+    alert(`Estuve en ${brazilCity} y me acordé de ti`);
+}
+
+function alertaButton() {
+    alert("Yo amo JS");
+}
+
+function sumaButton() {
+    let number1 = parseInt(prompt("Escribe un número para hacer una suma"));
+    let number2 = parseInt(prompt("Escribe otro número para hacer una suma"));
+    let resultado = number1 + number2;
+
+    alert(`El resultado de la suma es ${resultado}`);
+
+}
